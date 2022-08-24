61de314db78f92b6c833db7723ec29ea67c8730d
Niels H <hazcod@users.noreply.github.com>
2022-08-24 08:35:21 +0200

    fix: http logging sigsev

--- pkg/api/http_mw.go
+++ pkg/api/http_mw.go
@@ -15,19 +15,19 @@ type TaggedRoundTripper struct {
 	Logger  *logrus.Logger
 }
 
-func (t TaggedRoundTripper) RoundTrip(req *http.Request) (res *http.Response, e error) {
+func (t TaggedRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	req.Header.Set("user-agent", clientTag)
 	resp, err := t.Proxied.RoundTrip(req)
 
 	if t.Logger != nil && t.Logger.IsLevelEnabled(logrus.TraceLevel) {
-		dumped, err := httputil.DumpRequest(resp.Request, true)
+		dumped, err := httputil.DumpRequest(req, true)
 		if err != nil {
 			t.Logger.WithError(err).Error("could not dump http request")
 		} else {
 			t.Logger.Trace(string(dumped))
 		}
 
-		dumped, err = httputil.DumpResponse(resp, true)
+		dumped, err = httputil.DumpResponse(req.Response, true)
 		if err != nil {
 			t.Logger.WithError(err).Error("could not dump http response")
 		} else {
